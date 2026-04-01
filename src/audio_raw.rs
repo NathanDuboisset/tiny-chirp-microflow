99ed4b60b65519e6a6c596f64ce016d54517402c
NathanDuboisset <nathan.duboisset@gmail.com>
2026-04-01 12:11:10 +0200

    fix and change default

--- src/audio_raw.rs
+++ src/audio_raw.rs
@@ -1,17 +1,20 @@
 use microflow::buffer::Buffer4D;
+use nalgebra::SMatrix;
 
 pub const AUDIO_LEN: usize = (super::spectrogram::FRAMES - 1) * super::spectrogram::FRAME_STEP + super::spectrogram::FRAME_LENGTH;
 
+type AudioImage = SMatrix<[f32; 1], AUDIO_LEN, 1>;
+
 pub fn prepare(audio: &[f32]) -> Buffer4D<f32, 1, AUDIO_LEN, 1, 1> {
-    let mut buf = Buffer4D::from_element(0.0f32);
+    let mut out: AudioImage = SMatrix::from_element([0.0f32; 1]);
 
     let mut i = 0usize;
     while i < AUDIO_LEN {
         let sample = if i < audio.len() { audio[i] } else { 0.0 };
-        buf[(0, i, 0, 0)] = sample;
+        out[(i, 0)] = [sample];
         i += 1;
     }
 
-    buf
+    [out]
 }
 
